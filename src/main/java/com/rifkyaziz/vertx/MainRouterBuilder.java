5eea6cdd8dd219fda9c23da02e58e00119bf99e4
rifkyaziz <rifkyaziz@yandex.com>
2018-11-09 18:08:55 +0700

    fix no class exception

--- src/main/java/com/rifkyaziz/vertx/MainRouterBuilder.java
+++ src/main/java/com/rifkyaziz/vertx/MainRouterBuilder.java
@@ -2,7 +2,6 @@ package com.rifkyaziz.vertx;
 
 import com.rifkyaziz.vertx.handler.MainHandler;
 import io.vertx.core.Vertx;
-import io.vertx.core.http.HttpServerResponse;
 import io.vertx.ext.web.Router;
 
 class MainRouterBuilder {
